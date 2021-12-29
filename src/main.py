6662bf361b6eb0c70270a1d85be83d5f963871a8
samir benzada <samirbenzada@samirs-MBP.attlocal.net>
2021-12-29 10:31:28 -0500

    new background

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ def handle_invalid_usage(error):
 
 @app.route('/')
 def hello_world():
-    return "<div style='text-align: center; background-color: orange'><h1>Backend running...</h1><br/><h3>Welcome back samir</h3><img src='https://media.gettyimages.com/photos/woman-sitting-by-washing-machine-picture-id117852649?s=2048x2048' width='80%' /></div>"
+    return "<div style='text-align: center; background-color: orange'><h1>Backend running...</h1><br/><h3>Welcome back samir</h3><img src='https://cdn.pixabay.com/photo/2020/01/10/15/11/nude-4755496_1280.jpg' width='80%' /></div>"
 
 @app.route('/login', methods=['POST'])
 def handle_login():
