40864d1a0e2d13f2624feda2e4af79e10477864c
DNiev <danny.nieves411@gmail.com>
2022-01-19 21:18:30 -0500

    tag reader is functional. needs error outlier handling

--- src/reader.py
+++ src/reader.py
@@ -1,10 +1,13 @@
 import cv2
 import imutils
 import numpy as np
+import pytesseract
 
 #read and resize image
 img = cv2.imread('/Users/danny/Desktop/Private Dev Projects/Valet/backend/payload/lptest.jpeg', cv2.IMREAD_COLOR)
 img = cv2.resize(img, (620,480))
+
+# test above code with ResizeWithAspectRatio function enabled
 
 # convert image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -19,6 +22,8 @@ contours = imutils.grab_contours(contours)
 contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 screenCnt = None
 
+# need to create a way to handle null errors when no enclosed contours are found
+
 # loop through results to find the rectangle
 for c in contours:
     peri = cv2.arcLength(c, True)
@@ -32,5 +37,18 @@ mask = np.zeros(gray.shape, np.uint8)
 new_image = cv2.drawContours(mask, [screenCnt], 0, 255, -1,)
 new_image = cv2.bitwise_and(img, img, mask=mask)
 
+# crop the tag out of the image
+(x, y) = np.where(mask == 255)
+(topx, topy) = (np.min(x), np.min(y))
+(bottomx, bottomy) = (np.max(x), np.max(y))
+cropped = new_image[topx:bottomx + 1, topy:bottomy + 1]
+
+# read the tag
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Tesseract-OCR\tesseract.exe'
+text = pytesseract.image_to_string(cropped, config = '--psm 11')
+print("Got your tag, bitch!:", text)
+
 # the following line of code is used only for testing
-# cv2.imwrite('masked.jpg', new_image)
+# cv2.imwrite('cropped.jpg', cropped)
+
+# need to develop a way to handle failures
